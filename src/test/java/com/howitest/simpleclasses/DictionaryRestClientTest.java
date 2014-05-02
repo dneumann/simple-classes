ea512422d5322b57c60b3907c71f40ffd9e8a9f0
dennis <dennis@noname>
2014-05-02 17:30:10 +0200

    finished tests

--- src/test/java/com/howitest/simpleclasses/DictionaryRestClientTest.java
+++ src/test/java/com/howitest/simpleclasses/DictionaryRestClientTest.java
@@ -2,20 +2,44 @@ package com.howitest.simpleclasses;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.hamcrest.CoreMatchers.containsString;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class DictionaryRestClientTest {
+	
+	private Http httpMock;
+	private DictionaryRestClient clientSut;
 
+	@Before
+	public void beforeEachTest() {
+		httpMock = mock(Http.class);
+		clientSut = new DictionaryRestClient();
+		clientSut.setHttp(httpMock);
+	}
+	
 	@Test
 	public void shouldAddWordSuccessfully() {
-		Http httpMock = mock(Http.class);
 		when(httpMock.submitPut("mock", "Attrappe")).thenReturn("OK");
-		DictionaryRestClient client = new DictionaryRestClient();
-		client.setHttp(httpMock);
+		String returnedMessage = clientSut.addWord("mock", "Attrappe");
 		
-		String returnedMessage = client.addWord("mock", "Attrappe");
 		assertEquals("Word added: mock. Translation: Attrappe.", returnedMessage);
+	}
+
+	@Test
+	public void shouldFailWithAnErrorMessage() {
+		when(httpMock.submitPut(anyString(), anyString())).thenReturn("No connection to server.");
+		String returnedMessage = clientSut.addWord("mock", "Attrappe");
+		
+		assertThat(returnedMessage, containsString("Reason: No connection to server."));
+	}
+	
+	@Test
+	public void shouldCallTheRightMethod() {
+		clientSut.addWord("mock", "Attrappe");
+		
+		verify(httpMock).submitPut("mock", "Attrappe");
 	}
 
 }
